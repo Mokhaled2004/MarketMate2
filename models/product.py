c1f48522ff00d7b9fb1441ee5906f17939809f35
Mokhaled2004 <136094982+Mokhaled2004@users.noreply.github.com>
2024-06-22 12:04:19 +0300

    Update product.py

---
+++ models/product.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Defines the Product  class."""
+
+from Models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Table, String, Integer, Float, ForeignKey
+from Models.order import order_product
+
+
+
+
+
+class Product(BaseModel):
+    
+    """Represents a product """
+    __tablename__ = "products"
+
+    product_id = Column(Integer, primary_key=True, nullable=False)
+    order_id = Column(Integer, ForeignKey('orders.order_id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    price = Column(Float, nullable=False)
+    category = Column(String(128), nullable=False)
+    stock = Column(Integer, nullable=False)
+    order_product = relationship("Order", secondary="order_product")
+
+
+
+    
