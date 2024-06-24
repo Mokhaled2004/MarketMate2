66fd0779f5fdabbc6804c84cae53f9a6de6a872f
Mokhaled2004 <mokhaled2004@gmail.com>
2024-06-24 12:49:55 +0300

    YASEEDYYYYY

--- web_flask/main.py
+++ web_flask/main.py
@@ -59,36 +59,35 @@ def payment():
 def paymentconfirm():
     return render_template('Payment Confirmation HTML and CSS.html', title='Payment Confirmation')
 
-@app.route('/category/fruits')
+@app.route('/fruits')
 def fruits():
     return render_template('Fruits Category Page HTML.html', title='Fruits & Vegetables')
 
-@app.route('/category/medicine')
+@app.route('/medicine')
 def medicine():
     return render_template('Medicine Category Page HTML.html', title='Medicine')
 
-@app.route('/category/babycare')
+
+@app.route('/babycare')
 def babycare():
     return render_template('Baby Care Category Page HTML.html', title='Baby Care')
 
-@app.route('/category/meat')
+@app.route('/meat')
 def meat():
     return render_template('Meat Category Page HTML.html', title='Meat')
 
-@app.route('/category/bakery')
+
+@app.route('/bakery')
 def bakery():
     return render_template('Bakery Category Page HTML.html', title='Bakery')
 
-@app.route('/category/snacks')
+@app.route('/snacks')
 def snacks():
     return render_template('Snacks Category Page HTML.html', title='Snacks')
 
-@app.route('/category/dairy')
+@app.route('/dairy')
 def dairy():
     return render_template('Dairy Category Page HTML.html', title='Dairy Products')
-
-
-
 
 
 if __name__ == "__main__":
