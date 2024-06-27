51fbbbcde40900ef97596773d7d96acc58ee6a93
mazenwael <114602580+mazenwaelx@users.noreply.github.com>
2024-06-27 12:32:03 +0000

    lol

--- web_flask/static/js/Cart Test JS.js
+++ web_flask/static/js/Cart Test JS.js
@@ -91,87 +91,385 @@ const meatProducts = [
     {
         id: 21,
         image: '../static/images/images/beef.jpg',
-        title: 'beef',
-        price: 10,
+        title: 'Beef',
+        price: 350,
     },
     {
         id: 22,
-        image: '../static/images/images/chicken.jpg',
-        title: 'chicken',
-        price: 8,
+        image: '../static/images/images/lamb.jpg',
+        title: 'Lamb',
+        price: 600,
     },
     {
         id: 23,
-        image: '../static/images/images/lamb.jpg',
-        title: 'lamb',
-        price: 15,
+        image: '../static/images/images/vension.jpg',
+        title: 'Vension',
+        price: 650,
     },
     {
         id: 24,
         image: '../static/images/images/goat.jpg',
-        title: 'goat',
-        price: 15,
+        title: 'Goat',
+        price: 400,
+    },
+    {
+        id: 25,
+        image: '../static/images/images/chicken.jpg',
+        title: 'Chicken',
+        price: 250,
+    },
+    {
+        id: 26,
+        image: '../static/images/images/duck.jpg',
+        title: 'Duck',
+        price: 230,
+    },
+    {
+        id: 27,
+        image: '../static/images/images/rabbit.jpg',
+        title: 'Rabbit',
+        price: 330,
+    },
+    {
+        id: 28,
+        image: '../static/images/images/liver.jpg',
+        title: 'Liver',
+        price: 350,
+    },
+    {
+        id: 29,
+        image: '../static/images/images/sausage.jpg',
+        title: 'Sausage',
+        price: 230,
+    },
+    {
+        id: 30,
+        image: '../static/images/images/tuna.jpg',
+        title: 'Tuna',
+        price: 400,
+    },
+    {
+        id: 31,
+        image: '../static/images/images/trout.jpg',
+        title: 'Trout',
+        price: 700,
+    },
+    {
+        id: 32,
+        image: '../static/images/images/mackerel.jpg',
+        title: 'Mackerel',
+        price: 250,
     }
 ];
 
 // Adding meat products to the existing fruitVegetableProducts array
 const bakeryProducts = [
     {
-        id: 31,
-        image: '../static/images/images/whitebread.jpg',
-        title: 'whitebread',
-        price: 10,
-    },
-    {
-        id: 32,
-        image: '../static/images/images/ryebread.jpg',
-        title: 'Ryebread',
-        price: 8,
-    },
-    {
         id: 33,
-        image: '../static/images/images/sourdoughbread.jpg',
-        title: 'Sourdoughbread',
-        price: 15,
+        image: '../static/images/images/whitebread.jpg',
+        title: 'White Bread',
+        price: 80,
     },
     {
         id: 34,
+        image: '../static/images/images/ryebread.jpg',
+        title: 'Rye Bread',
+        price: 120,
+    },
+    {
+        id: 35,
+        image: '../static/images/images/baguette.jpg',
+        title: 'Baguette Bread',
+        price: 70,
+    },
+    {
+        id: 36,
+        image: '../static/images/images/sourdoughbread.jpg',
+        title: 'Sourdough Bread',
+        price: 100,
+    },
+    {
+        id: 37,
         image: '../static/images/images/croissant.jpg',
         title: 'Croissant',
-        price: 15,
+        price: 40,
+    },
+    {
+        id: 38,
+        image: '../static/images/images/turnover.jpg',
+        title: 'Turnover',
+        price: 70,
+    },
+    {
+        id: 39,
+        image: '../static/images/images/spongecake.jpg',
+        title: 'Sponge Cake',
+        price: 300,
+    },
+    {
+        id: 40,
+        image: '../static/images/images/cheesecake.jpg',
+        title: 'Cheese Cake',
+        price: 250,
+    },
+    {
+        id: 41,
+        image: '../static/images/images/cookies.jpg',
+        title: 'Cookies',
+        price: 165,
+    },
+    {
+        id: 42,
+        image: '../static/images/images/shortbread.jpg',
+        title: 'Short Bread',
+        price: 135,
+    },
+    {
+        id: 43,
+        image: '../static/images/images/applepie.jpg',
+        title: 'Apple Pie',
+        price: 220,
+    },
+    {
+        id: 44,
+        image: '../static/images/images/donut.jpg',
+        title: 'Donut',
+        price: 45,
     }
 ];
+
 
 // Adding meat products to the existing fruitVegetableProducts array
 const snacksProducts = [
     {
-        id: 41,
+        id: 45,
         image: '../static/images/images/chips.jpg',
         title: 'Chips',
+        price: 15,
+    },
+    {
+        id: 46,
+        image: '../static/images/images/pretzels.jpg',
+        title: 'Pretzels',
+        price: 80,
+    },
+    {
+        id: 47,
+        image: '../static/images/images/popcorn.jpg',
+        title: 'Popcorn',
+        price: 40,
+    },
+    {
+        id: 48,
+        image: '../static/images/images/crackers.jpg',
+        title: 'Crackers',
+        price: 25,
+    },
+    {
+        id: 49,
+        image: '../static/images/images/trail mix.jpg',
+        title: 'Trail Mix',
+        price: 230,
+    },
+    {
+        id: 50,
+        image: '../static/images/images/sweets.jpg',
+        title: 'Candy',
+        price: 175,
+    },
+    {
+        id: 51,
+        image: '../static/images/images/icecream.jpg',
+        title: 'Ice Cream',
+        price: 35,
+    },
+    {
+        id: 52,
+        image: '../static/images/images/waterbottle.jpg',
+        title: 'Water',
         price: 10,
     },
     {
-        id: 42,
-        image: '../static/images/images/pretzels.jpg',
-        title: 'Pretzels',
-        price: 8,
-    },
-    {
-        id: 43,
-        image: '../static/images/images/popcorn.jpg',
-        title: 'Popcorn',
+        id: 53,
+        image: '../static/images/images/orangejuice.jpg',
+        title: 'Orange Juice',
         price: 15,
     },
     {
-        id: 44,
-        image: '../static/images/images/crackers.jpg',
-        title: 'Crackers',
+        id: 54,
+        image: '../static/images/images/applejuice.jpg',
+        title: 'Apple Juice',
         price: 15,
+    },
+    {
+        id: 55,
+        image: '../static/images/images/soda.jpg',
+        title: 'Sodas',
+        price: 20,
+    },
+    {
+        id: 56,
+        image: '../static/images/images/redbull.jpg',
+        title: 'Redbull',
+        price: 45,
+    }
+];
+const dairyProducts = [
+    {
+        id: 57,
+        image: '../static/images/images/Milk.jpeg',
+        title: 'Milk',
+        price: 40,
+    },
+    {
+        id: 58,
+        image: '../static/images/images/Feta.jpeg',
+        title: 'Feta Cheese',
+        price: 20,
+    },
+    {
+        id: 59,
+        image: '../static/images/images/Cheddar.jpeg',
+        title: 'Cheddar Cheese',
+        price: 30,
+    },
+    {
+        id: 60,
+        image: '../static/images/images/Mozzarella.jpeg',
+        title: 'Mozzarella Cheese',
+        price: 35,
+    },
+    {
+        id: 61,
+        image: '../static/images/images/Butter.jpeg',
+        title: 'Butter',
+        price: 50,
+    },
+    {
+        id: 62,
+        image: '../static/images/images/Yogurt.jpeg',
+        title: 'Yogurt',
+        price: 25,
+    },
+    {
+        id: 63,
+        image: '../static/images/images/Whipping cream.jpeg',
+        title: 'Whipping cream',
+        price: 35,
+    },
+    {
+        id: 64,
+        image: '../static/images/images/Ghee.jpeg',
+        title: 'Ghee',
+        price: 45,
+    },
+    {
+        id: 65,
+        image: '../static/images/images/Cottage Cheese.jpeg',
+        title: 'Cottage Cheese',
+        price: 20,
+    }
+];
+const babyProducts = [
+    {
+        id: 66,
+        image: '../static/images/johnsonoil.jpg',
+        title: "Johnson's Baby Oil",
+        price: 350,
+    },
+    {
+        id: 67,
+        image: '../static/images/Baby Wipes.jpg',
+        title: "Little's Baby Wipes",
+        price: 70,
+    },
+    {
+        id: 68,
+        image: '../static/images/Moisturizer.jpg',
+        title: 'Moisturizer',
+        price: 470,
+    },
+    {
+        id: 69,
+        image: '../static/images/Baby Shampoo.jpg',
+        title: 'Himalaya Baby Shampoo',
+        price: 330,
+    },
+    {
+        id: 70,
+        image: '../static/images/Baby Powder.jpg',
+        title: "Johnson's Baby Powder",
+        price: 125,
+    },
+    {
+        id: 71,
+        image: '../static/images/Diapers.jpg',
+        title: 'Pampers Baby Pants',
+        price: 550,
+    }
+];
+const medicineProducts = [
+    {
+        id: 72,
+        image: '../static/images/images/panadol.jpg',
+        title: 'Panadol',
+        price: 60,
+    },
+    {
+        id: 73,
+        image: '../static/images/images/bruffen.jpg',
+        title: 'Bruffen',
+        price: 40,
+    },
+    {
+        id: 74,
+        image: '../static/images/images/zyrtec.jpg',
+        title: 'Zyrtec',
+        price: 35,
+    },
+    {
+        id: 75,
+        image: '../static/images/images/cataflam.jpg',
+        title: 'Cataflam',
+        price: 30,
+    },
+    {
+        id: 76,
+        image: '../static/images/images/osteocare.jpg',
+        title: 'Osteocare',
+        price: 70,
+    },
+    {
+        id: 77,
+        image: '../static/images/images/otrivin.png',
+        title: 'Otrivin',
+        price: 45,
+    },
+    {
+        id: 78,
+        image: '../static/images/images/feroglobin.png',
+        title: 'Feroglobin',
+        price: 45,
+    },
+    {
+        id: 79,
+        image: '../static/images/images/aspirin.jpg',
+        title: 'Aspirin',
+        price: 70,
+    },
+    {
+        id: 80,
+        image: '../static/images/images/motilium.jpg',
+        title: 'Motilium',
+        price: 45,
     }
 ];
 
+
+
+
 // Merge fruitVegetableProducts and meatProducts into a single categories array
-const categories = [...fruitVegetableProducts, ...meatProducts, ...bakeryProducts, ...snacksProducts];
+const categories = [...fruitVegetableProducts, ...meatProducts, ...bakeryProducts, ...snacksProducts, ...dairyProducts, ...babyProducts, ...medicineProducts];
 
 let i = 0;
 async function fetchProducts() {
@@ -202,6 +500,15 @@ function switchCategory(category) {
     else if (category === 'snacksProducts') {
         document.getElementById('categoryTitle').textContent = 'SnacksProducts';
     }
+    else if (category === 'dairyProducts') {
+        document.getElementById('categoryTitle').textContent = 'Dairy Products';
+    }
+    else if (category === 'babyProducts') {
+        document.getElementById('categoryTitle').textContent = 'Baby Products';
+    }
+    else if (category === 'medicineProducts') {
+        document.getElementById('categoryTitle').textContent = 'Medicine Products';
+    }
     
     displayProducts(category);
 }
@@ -220,6 +527,12 @@ function displayProducts(category) {
         products = bakeryProducts;
     } else if (category === 'snacksProducts') {
         products = snacksProducts;
+    }else if (category === 'dairyProducts') {
+        products = dairyProducts;
+    }else if(category === 'babyProducts') {
+        products = babyProducts;
+    }else if(category === 'medicineProducts') {
+        products = medicineProducts;
     }else {
         products = [];
     }
