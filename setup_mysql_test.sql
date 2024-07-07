74808e2a8b66c8da4d78065b9b75fdcf0e5ab546
Mokhaled2004 <mokhaled2004@gmail.com>
2024-07-07 20:53:55 +0300

    updated

--- setup_mysql_test.sql
+++
@@ -1,6 +0,0 @@
--- Prepares a MySQL Test server for the project.
-
-CREATE DATABASE IF NOT EXISTS marketmate_test_db;
-CREATE USER IF NOT EXISTS 'marketmate_test'@'localhost' IDENTIFIED BY 'marketmate_test_pwd';
-GRANT ALL PRIVILEGES ON marketmate_test_db . * TO 'marketmate_test'@'localhost';
-GRANT SELECT ON performance_schema . * TO 'marketmate_test'@'localhost';
